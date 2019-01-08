d1bddc14525e101088c18e3a53aee3acd2e8d601
zhaojing <zhaojing@comp.nus.edu.sg>
2019-01-08 11:17:23 +0800

    momentum correlation finishes

--- examples/residual-knowledge-driven-example/mlp_residual_hook_resreg.py
+++ examples/residual-knowledge-driven-example/mlp_residual_hook_resreg.py
@@ -232,9 +232,9 @@ def train_validate_test_model(model, train_loader, test_loader, criterion, optim
                 print ('final test loss = %f, test accuracy = %f, test macro auc = %f, test micro auc = %f'%(loss.data[0], accuracy, macro_auc, micro_auc))
 '''
 
-def train_validate_test_resmlp_model_MNIST(model_name, model, gpu_id, train_loader, test_loader, criterion, optimizer, reg_lambda, weightdecay, max_epoch=25):
+def train_validate_test_resmlp_model_MNIST(model_name, model, gpu_id, train_loader, test_loader, criterion, optimizer, reg_lambda, momentum_mu, hidden_dim, weightdecay, max_epoch=25):
     logger = logging.getLogger('res_reg')
-    res_regularizer_instance = ResRegularizer(reg_lambda=reg_lambda)
+    res_regularizer_instance = ResRegularizer(reg_lambda=reg_lambda, momentum_mu=momentum_mu, feature_dim=hidden_dim)
     # hyper parameters
     print('Beginning Training')
     since = time.time()
@@ -414,9 +414,10 @@ if __name__ == '__main__':
     # Train and evaluate
     # train_validate_test_model(model_ft, gpu_id, train_loader, test_loader, criterion, optimizer_ft, max_epoch=args.maxepoch)
     reg_lambda = 0.1 # resreg strength
+    momentum_mu = 0.9 # momentum mu
     weightdecay = 0.1 # other parameters' weight decay
     # Train and evaluate MNIST on resmlp or mlp model
-    train_validate_test_resmlp_model_MNIST(args.modelname, model_ft, gpu_id, train_loader, test_loader, criterion, optimizer_ft, reg_lambda, weightdecay, max_epoch=args.maxepoch)
+    train_validate_test_resmlp_model_MNIST(args.modelname, model_ft, gpu_id, train_loader, test_loader, criterion, optimizer_ft, reg_lambda, momentum_mu, dim_vec[1], weightdecay, max_epoch=args.maxepoch)
 
 # python mlp_residual_hook_resreg.py -datadir . -modelname regresnetmlp3 -blocks 3 -batchsize 64 -maxepoch 10 -gpuid 1
 # python mlp_residual_hook_resreg.py -datadir . -modelname resnetmlp3 -blocks 3 -batchsize 64 -maxepoch 10 -gpuid 1
--- examples/residual-knowledge-driven-example/res_regularizer.py
+++ examples/residual-knowledge-driven-example/res_regularizer.py
@@ -8,9 +8,13 @@ class ResRegularizer():
     Args:
         hyperparameters: a, b, alpha (like the coefficient of L2), uptfreq
     '''
-    def __init__(self, reg_lambda=None):
+    def __init__(self, reg_lambda=None, momentum_mu=None, feature_dim=None):
         self.reg_lambda = reg_lambda
+        self.momentum_mu = momentum_mu
         print ("self.reg_lambda: ", self.reg_lambda)
+        print ("new self.momentum_mu: ", self.momentum_mu)
+        self.correlation_moving_average = np.zeros((feature_dim, feature_dim))
+        print ('new self.correlation_moving_average shape: ', self.correlation_moving_average.shape)
     
     # calc correlation using one layer 
     def calcCorrelation(self):
@@ -23,9 +27,9 @@ class ResRegularizer():
     def calcCorrelation_two_layers(self):
         logger = logging.getLogger('res_reg')
         feature_dim = self.feature_matrix.shape[1]
-        logger.debug ("using two layers slef.feature_dim: %d", feature_dim)
+        logger.debug ("new using two layers self.feature_dim: %d", feature_dim)
         self.feature_correlation = np.corrcoef(self.feature_matrix, self.second_feature_matrix, rowvar=False)[feature_dim:, 0:feature_dim]
-        logger.debug ("using two layers slef.feature_correlation.shape:")
+        logger.debug ("new using two layers self.feature_correlation.shape:")
         logger.debug (self.feature_correlation.shape)
 
     # singa: (word_num, doc_num)
@@ -39,6 +43,17 @@ class ResRegularizer():
         logger.debug ("reg_grad_w shape:")
         logger.debug (reg_grad_w.shape)
         return reg_grad_w
+    
+    # calculate regularization using self.correlation_moving_average
+    def calcRegGradAvg(self):
+        logger = logging.getLogger('res_reg')
+        correlation_abs_matrix = np.abs(self.correlation_moving_average) # only this line is different
+        correlation_abs_avg = np.mean(correlation_abs_matrix)
+        correlation_diff_matrix = correlation_abs_avg - correlation_abs_matrix
+        reg_grad_w = 2 * self.reg_lambda * np.exp(correlation_diff_matrix) * self.w_array
+        logger.debug ("new reg_grad_w shape:")
+        logger.debug (reg_grad_w.shape)
+        return reg_grad_w
 
 
     def apply(self, gpu_id, features, feature_idx, reg_lambda, epoch, param, name, step):
@@ -50,6 +65,7 @@ class ResRegularizer():
         logger.debug ("self.feature_matrix shape:")
         logger.debug (self.feature_matrix.shape)
         logger.debug ("self.feature_matrix norm: %f", np.linalg.norm(self.feature_matrix))
+        logger.debug ("new self.second_feature_matrix norm: %f", np.linalg.norm(self.second_feature_matrix))
         self.reg_lambda = reg_lambda
         logger.debug ("self.reg_lambda: %f", self.reg_lambda)
         self.w_array = param.data.cpu().numpy()
@@ -57,7 +73,9 @@ class ResRegularizer():
         logger.debug (self.w_array.shape)
         # self.calcCorrelation()
         self.calcCorrelation_two_layers()
-        self.reg_grad_w = self.calcRegGrad()
+        self.correlation_moving_average = self.momentum_mu * self.correlation_moving_average + (1-self.momentum_mu) * self.feature_correlation
+        # self.reg_grad_w = self.calcRegGrad()
+        self.reg_grad_w = self.calcRegGradAvg()
         reg_grad_w_dev = (torch.from_numpy(self.reg_grad_w)).float()
         logger.debug ("step: %d", step)
         logger.debug ("name: " +  name)
