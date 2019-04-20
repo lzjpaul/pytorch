059a70cb57ab6fa159b2f70c6d1c7547aef32b83
zhaojing <zhaojing@comp.nus.edu.sg>
2019-04-20 20:58:29 +0800

    19-4-20 check wlm convergence

--- examples/residual-knowledge-driven-example-test-lda-prior/archive-code/19-2-27/readme.md
+++ examples/residual-knowledge-driven-example-test-lda-prior/archive-code/19-2-27/readme.md
@@ -1,2 +1,6 @@
 (1) 19-2-27
 temporarily used as lda-prior folder
+
+(2) 19-3-2
+(2-1) wlm model code
+(2-2) data.py --> data_origin.py (original example)
---
+++ examples/residual-knowledge-driven-example-test-lda-prior/archive-code/19-4-20/19-4-20-wlm-converge-c0.sh
@@ -0,0 +1,4 @@
+# running folder: /home/zhaojing/residual-knowledge-driven/pytorch/examples/residual-knowledge-driven-example-test-lda-prior
+# running machine: ncra-ncrc
+# ncrc0
+CUDA_VISIBLE_DEVICES=0 python train_lstm_main_hook_resreg_real_wlm_tune_converge.py -traindatadir ./data/wikitext-2 -trainlabeldir ./data/wikitext-2 -testdatadir ./data/wikitext-2 -testlabeldir ./data/wikitext-2 -seqnum 35 -modelname lstm -blocks 3 -lr 20.0 -decay 0.000001 -reglambda 0.0 -batchsize 100 -regmethod 6 -firstepochs 0 -considerlabelnum 1 -maxepoch 4000 -gpuid 0 --priorbeta 0.0 --emsize 200 --nhid 200 --clip 0.2615 --seed 1111 | tee -a /hdd1/zhaojing/res-regularization/19-4-20/4-20-wlm-converge-4000epoch-6
