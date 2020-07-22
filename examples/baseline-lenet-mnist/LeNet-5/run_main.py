b0a9071796ab5dba6c2ca22426bedd253a431951
zhaojing <zhaojing@comp.nus.edu.sg>
2020-07-22 17:42:37 +0800

    mnist lenet main

---
+++ examples/baseline-lenet-mnist/LeNet-5/run_main.py
@@ -0,0 +1,113 @@
+from lenet import LeNet5
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision.datasets.mnist import MNIST
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import visdom
+import onnx
+import numpy as np
+
+# viz = visdom.Visdom()
+# cur_batch_win = None
+
+def train(epoch, net, data_train_loader, optimizer, criterion):
+    # global cur_batch_win
+    net.train()
+    loss_list, batch_list = [], []
+    for i, (images, labels) in enumerate(data_train_loader):
+        optimizer.zero_grad()
+
+        output = net(images)
+
+        loss = criterion(output, labels)
+
+        loss_list.append(loss.detach().cpu().item())
+        batch_list.append(i+1)
+
+        if i % 10 == 0:
+            print('Train - Epoch %d, Batch: %d, Loss: %f' % (epoch, i, loss.detach().cpu().item()))
+
+        # Update Visualization
+        """
+        if viz.check_connection():
+            cur_batch_win = viz.line(torch.Tensor(loss_list), torch.Tensor(batch_list),
+                                     win=cur_batch_win, name='current_batch_loss',
+                                     update=(None if cur_batch_win is None else 'replace'),
+                                     opts=cur_batch_win_opts)
+        """
+
+        loss.backward()
+        ### print norm
+        if i == 0:
+            for name, f in net.named_parameters():
+                print ('param name: ', name)
+                print ('param size:', f.data.size())
+                print ('param norm: ', np.linalg.norm(f.data.cpu().numpy()))
+                print ('lr 1.0 * param grad norm: ', np.linalg.norm(f.grad.data.cpu().numpy() * 1.0))
+        ### print norm
+        optimizer.step()
+
+
+def test(net, data_test_loader, data_test, criterion):
+    net.eval()
+    total_correct = 0
+    avg_loss = 0.0
+    for i, (images, labels) in enumerate(data_test_loader):
+        output = net(images)
+        avg_loss += criterion(output, labels).sum()
+        pred = output.detach().max(1)[1]
+        total_correct += pred.eq(labels.view_as(pred)).sum()
+
+    avg_loss /= len(data_test)
+    print('Test Avg. Loss: %f, Accuracy: %f' % (avg_loss.detach().cpu().item(), float(total_correct) / len(data_test)))
+
+
+def train_and_test(epoch, net, data_train_loader, data_test_loader, data_test, optimizer, criterion):
+    train(epoch, net, data_train_loader, optimizer, criterion)
+    test(net, data_test_loader, data_test, criterion)
+
+    dummy_input = torch.randn(1, 1, 32, 32, requires_grad=True)
+    # print ("before onnx export")
+    torch.onnx.export(net, dummy_input, "lenet.onnx")
+
+    onnx_model = onnx.load("lenet.onnx")
+    # print ("before check model")
+    # onnx.checker.check_model(onnx_model)
+    print ("not check model??")
+
+
+def main():
+    data_train = MNIST('./data/mnist',
+                   download=True,
+                   transform=transforms.Compose([
+                       transforms.Resize((32, 32)),
+                       transforms.ToTensor()]))
+    data_test = MNIST('./data/mnist',
+                      train=False,
+                      download=True,
+                      transform=transforms.Compose([
+                          transforms.Resize((32, 32)),
+                          transforms.ToTensor()]))
+    data_train_loader = DataLoader(data_train, batch_size=256, shuffle=True, num_workers=8)
+    data_test_loader = DataLoader(data_test, batch_size=1024, num_workers=8)
+
+    net = LeNet5()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=2e-3)
+    """
+    cur_batch_win_opts = {
+        'title': 'Epoch Loss Trace',
+        'xlabel': 'Batch Number',
+        'ylabel': 'Loss',
+        'width': 1200,
+        'height': 600,
+    }
+    """
+    for e in range(1, 16):
+        train_and_test(e, net, data_train_loader, data_test_loader, data_test, optimizer, criterion)
+
+
+if __name__ == '__main__':
+    main()
